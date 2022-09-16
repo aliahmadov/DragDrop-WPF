b8b99e46f33f44a1db11c355c973333a7b8e6d54
Ali Ahmadov <aahmadov7626@ada.edu.az>
2022-09-16 12:16:38 +0400

    first commit

---
+++ DragDrop_Image/ImageFrame.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragDrop_Image
+{
+    /// <summary>
+    /// Interaction logic for ImageFrame.xaml
+    /// </summary>
+    public partial class ImageFrame : UserControl
+    {
+
+
+
+        public string FileName
+        {
+            get { return (string)GetValue(FileNameProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FileName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileNameProperty =
+            DependencyProperty.Register("FileName", typeof(string), typeof(ImageFrame), new PropertyMetadata(default(ImageFrame)));
+
+        public string ImageSource
+        {
+            get { return (string)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(string), typeof(ImageFrame), new PropertyMetadata(default(ImageFrame)));
+
+
+
+        public ImageFrame()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
---
+++ DragDrop_Image/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DragDrop_Image
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            listBox.SelectedIndex++;
+            var image = listBox.Items[IndexItem] as ImageFrame;
+            imageBrush.ImageSource = image.imgUC.Source;
+        }
+
+        public object MyItem { get; set; }
+
+        public int IndexItem { get; set; }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            if (open.ShowDialog() == true)
+            {
+                var file = open.FileName;
+
+                if (file.EndsWith(".jpg") || file.EndsWith(".png"))
+                {
+                    ImageFrame uc = new ImageFrame();
+                    uc.ImageSource = file;
+                    uc.FileName = open.FileName;
+                    listBox.Items.Add(uc);
+                }
+                else
+                {
+                    MessageBox.Show("Picture in wrong format - .jpg or .png", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+
+        }
+
+        private void WrapPanel_Drop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(typeof(ListBox)) as ListBox;
+            var image = data.Items[IndexItem] as ImageFrame;
+
+
+            imageBrush.ImageSource = image.imgUC.Source;
+        }
+
+        private void listBox_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(listBox, listBox, DragDropEffects.Move);
+            }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MyItem = listBox.SelectedItem;
+            IndexItem = listBox.SelectedIndex;
+        }
+
+        private void rightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.SelectedIndex++;
+            var image = listBox.Items[IndexItem] as ImageFrame;
+            imageBrush.ImageSource = image.imgUC.Source;
+        }
+
+        private void leftBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedIndex > 0)
+            {
+                listBox.SelectedIndex--;
+                var image = listBox.Items[IndexItem] as ImageFrame;
+                imageBrush.ImageSource = image.imgUC.Source;
+            }
+        }
+
+        DispatcherTimer timer = new DispatcherTimer();
+        private void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Interval = TimeSpan.FromSeconds(3);
+            timer.Tick += timer_tick;
+            timer.Start();
+        }
+
+        private void pauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+    }
+}
